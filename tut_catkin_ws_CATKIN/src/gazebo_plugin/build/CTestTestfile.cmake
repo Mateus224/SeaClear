ed668f1833a1ea6c3357302ec20117a6df4ab6d0
Mateus <m_rosynski@gmx.net>
2020-09-08 16:59:05 +0300

    added pointcloud

---
+++ tut_catkin_ws_CATKIN/src/gazebo_plugin/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mateus/seaclear/tut_catkin_ws_CATKIN/src/gazebo_plugin
+# Build directory: /home/mateus/seaclear/tut_catkin_ws_CATKIN/src/gazebo_plugin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ tut_catkin_ws_CATKIN/src/gazebo_plugin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/mateus/seaclear/tut_catkin_ws_CATKIN/src/gazebo_plugin/build/devel:/home/mateus/seaclear/tut_catkin_ws_CATKIN/devel:/opt/ros/melodic'
+export LD_LIBRARY_PATH='/home/mateus/seaclear/tut_catkin_ws_CATKIN/devel/lib:/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/home/mateus/seaclear/tut_catkin_ws_CATKIN/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mateus/seaclear/tut_catkin_ws_CATKIN/src/gazebo_plugin/build/devel/share/common-lisp:/home/mateus/seaclear/tut_catkin_ws_CATKIN/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/mateus/seaclear/tut_catkin_ws_CATKIN/src/gazebo_plugin:/home/mateus/seaclear/tut_catkin_ws_CATKIN/src:/opt/ros/melodic/share'
